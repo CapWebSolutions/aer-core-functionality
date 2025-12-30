411abcbc2435ef88610a1e484b3ccf435f24e031
Matt Ryan <matt@capwebsolutions.com>
2025-12-30 11:48:32 -0500

    V2 updates for site redesign.

--- aer-core-functionality.php
+++ aer-core-functionality.php
@@ -3,9 +3,11 @@
  * Plugin Name: AbbyERyan.com  Core Functionality
  * Plugin URI: https://github.com/CapWebSolutions/aer-core-functionality
  * Description: This contains all this site's core functionality so that it is theme independent. 
- * Version: 1.0.0
+ * Version: 2.0.0
  * Author: Cap Web Solutions
  * Author URI: https://capwebsolutions.com
+ * Github Plugin URI: https://github.com/CapWebSolutions/aer-core-functionality/
+
  *
  * @package      Core_Functionality
  * @since        1.0.0
@@ -22,43 +24,18 @@
 
   // Plugin Directory. Set constant so we know where we are installed.
   $plugin_url = plugin_dir_url( __FILE__ );
-  if ( is_ssl() ) {
-	  $plugin_url = str_replace( 'http://', 'https://', $plugin_url );
-  }
   define( 'CORE_FUNCTION_URL', $plugin_url );
   define( 'CORE_FUNCTION_DIR', plugin_dir_path( __FILE__ ) );
 
 // General. This should always be used. 
-include_once( CORE_FUNCTION_DIR . '/lib/functions/general.php' );
+// include_once( CORE_FUNCTION_DIR . '/lib/functions/general.php' );
 
 // Define needed Custom Post Types.
 include_once( CORE_FUNCTION_DIR . '/lib/functions/post-types.php' );
 
-// Define needed Custom Taxonomies.
-// include_once( CORE_FUNCTION_DIR . '/lib/functions/taxonomies.php' );
-
 // Define Custom Meta boxes.
 include_once( CORE_FUNCTION_DIR . '/lib/functions/metaboxes.php' );
 
-// Footer Setup.This should always be used. 
-// include_once( CORE_FUNCTION_DIR . '/lib/functions/core-footer.php' );
-
-// Woo tweaks. Only if WooCommerce active.
-// if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-// // or if ( class_exists( 'WooCommerce' ) ) {
-// 	include_once( CORE_FUNCTION_DIR . '/lib/functions/wootweaks.php' );
-// }
-
-
-// Gravity Forms tweaks. This should always be used if Gravity Forms active. Which one to use??
-if ( in_array( 'gravityforms/gravityforms.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	include_once( CORE_FUNCTION_DIR . '/lib/functions/gravitytweaks.php' );
-}
-// or 
-if ( class_exists( 'GFForms' ) ) { 
-	include_once( CORE_FUNCTION_DIR . '/lib/functions/gravitytweaks.php' );
-}
-
 // Set up templates for new post type
-add_filter( 'archive_template', 'aer_core_functionality_load_archive_template' );
-add_filter( 'single_template', 'aer_core_functionality_load_single_template' );
+// add_filter( 'archive_template', 'aer_core_functionality_load_archive_template' );
+// add_filter( 'single_template', 'aer_core_functionality_load_single_template' );
